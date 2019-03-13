993650be2065b7906b0164b8531256ec89bb7b18
Mohd Takreem <takreem@asagrisystems.com>
2019-03-13 12:51:05 +0530

    commanfunction for all service

---
+++ commanfunction/routes/commanFunction.js
@@ -0,0 +1,77 @@
+// 'use strict';
+var dateTime = require("node-datetime");
+var fs = require("fs");
+var dt = dateTime.create();
+var formattedDate = dt.format("Y-m-d");
+exports.gomosLog = function(x){
+    if(process.argv[3] >= arguments[0]){
+       var  currTime = new Date();
+      if(arguments[2] instanceof Object){
+        console.log(currTime.getHours()+":"+currTime.getMinutes()+":"+currTime.getSeconds()+"."+currTime.getMilliseconds()+"-"+arguments[1]);
+        console.log(arguments[2]);
+      }else if(arguments.length == 2){
+        console.log( currTime.getHours()+":"+currTime.getMinutes()+":"+currTime.getSeconds()+"."+currTime.getMilliseconds()+"-"+arguments[1]);
+      }
+      else{
+        console.log( currTime.getHours()+":"+currTime.getMinutes()+":"+currTime.getSeconds()+"."+currTime.getMilliseconds()+"-"+arguments[1]+" ["+arguments[2]+ "]");
+      }
+   
+    }
+  }
+ 
+  exports.unWantedLog = function(functionName,message){
+    var DateTime = new Date().toISOString();
+    let writeStream = fs.createWriteStream("../unWantedLogCommanlog-" +  formattedDate+ ".json", { flags: "a" });
+ var json ={DateTime,functionName ,message}
+ var jsonStrring = JSON.stringify(json)
+
+  // write some data with a base64 encoding
+  writeStream.write(
+
+    jsonStrring +
+    "\n"
+  );
+  
+  // the finish event is emitted when all data has been flushed from the stream
+  writeStream.on('finish', () => {  
+    console.log("wrote all data to file For Error");  
+  });
+  
+  // close the stream
+  writeStream.end(); 
+
+  }
+  exports.errorCustmHandler =  function(x){
+    // console.log(typeofError);
+      let writeStream = fs.createWriteStream("../commanError-" + formattedDate + ".json", { flags: "a" });
+      var dateTime = new Date().toISOString();
+    // write some data with a base64 encoding
+    // var errors = typeofError.toS
+    var errorobj ={
+      DateTime : dateTime,
+      serviceName: arguments[3],
+      functionName : arguments[0],
+      ErrorCode :arguments[1].statusCode,
+       Error : arguments[1].toString(),
+       typeofErrorstack:  arguments[1].stack
+  
+    }
+    if(arguments[2] != ''){
+      errorobj["message"] = arguments[2]
+    }
+    var strObj = JSON.stringify(errorobj)
+  // console.log(typeofError);
+    writeStream.write(
+      strObj+
+      "\n"
+      );
+    
+    // the finish event is emitted when all data has been flushed from the stream
+    writeStream.on('finish', () => {  
+        console.log('wrote all data to file');
+    });
+    // close the stream
+    writeStream.end(); 
+    }
+  
+
